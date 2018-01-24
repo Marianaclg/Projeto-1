3ac1b504875fce2a1e77422bf3515368bf48f65f
you@example.com <you@example.com>
2018-01-23 22:48:35 -0200

    first

---
+++ script.js
@@ -0,0 +1,11 @@
+let titulo = document.querySelector('h1');
+let texto = document.querySelector('p');
+
+function mudarTitulo() {
+  titulo.innerHTML = 'Sonho na Terra';
+}
+
+function mudarTexto() {
+  texto.style.color = 'red';
+  texto.style.backgroundColor = 'lightblue';
+}
